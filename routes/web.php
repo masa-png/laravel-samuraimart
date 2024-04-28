d017bf4e71c631ebf3df185864a0ee79e70361e4
masa-png <adidas.org@icloud.com>
2024-04-28 19:53:10 +0900

    カート機能を実装

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
@@ -49,5 +50,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('users/mypage/password/edit', 'edit_password')->name('mypage.edit_password');
         Route::put('users/mypage/password', 'update_password')->name('mypage.update_password');
         Route::get('users/mypage/favorite', 'favorite')->name('mypage.favorite');
+    });
+
+    Route::controller(CartController::class)->group(function () {
+        Route::get('users/carts', 'index')->name('carts.index');
+        Route::post('users/carts', 'store')->name('carts.store');
+        Route::delete('users/carts', 'destroy')->name('carts.destroy');
     });
 });
