198c47fb56f10353bb920f39a41c71f468fb5128
masa-png <adidas.org@icloud.com>
2024-04-29 16:32:23 +0900

    親カテゴリーのテーブルから取得するよう修正

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\MajorCategory;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function index()
     {
-        $categories = Category::all()->sortBy('major_category_name');
+        $categories = Category::all();
 
-        $major_category_names = Category::pluck('major_category_name')->unique();
+        $major_categories = MajorCategory::all();
 
-        return view('web.index', compact('major_category_names', 'categories'));
+        return view('web.index', compact('major_categories', 'categories'));
     }
 }
