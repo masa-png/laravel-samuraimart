6c231f2eb4dd0941ee366881d2c3177f516546d1
masa-png <adidas.org@icloud.com>
2024-05-01 22:54:57 +0900

    決済機能を実装

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,9 +2,91 @@
 
 namespace App\Http\Controllers;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 class CheckoutController extends Controller
 {
-    //
+    public function index()
+    {
+        $cart = Cart::instance(Auth::user()->id)->content();
+
+        $total = 0;
+        $has_carriage_cost = false;
+        $carriage_cost = 0;
+
+        foreach ($cart as $c) {
+            $total += $c->qty * $c->price;
+            if ($c->options->carriage) {
+                $has_carriage_cost = true;
+            }
+        }
+
+        if ($has_carriage_cost) {
+            $total += env('CARRIAGE');
+            $carriage_cost = env('CARRIAGE');
+        }
+
+        return view('carts.index', compact('cart', 'total', 'carriage_cost'));
+    }
+
+    public function store(Request $request)
+    {
+        $cart = Cart::instance(Auth::user()->id)->content();
+
+        $has_carriage_cost = false;
+
+        foreach ($cart as $product) {
+            if ($product->options->carriage) {
+                $has_carriage_cost = true;
+            }
+        }
+
+        Stripe::setApiKey((env('STRIPE_SECRET')));
+
+        $line_items = [];
+
+        foreach ($cart as $product) {
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'jpy',
+                    'product_data' => [
+                        'name' => $product->name,
+                    ],
+                    'unit_amount' => $product->price,
+                ],
+                'quantity' => $product->qty,
+            ];
+        }
+
+        if ($has_carriage_cost) {
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'jpy',
+                    'product_data' => [
+                        'name' => '送料',
+                    ],
+                    'unit_amount' => env('CARRIAGE'),
+                ],
+                'quantity' => 1,
+            ];
+        }
+
+        $checkout_session = Session::create([
+            'line_items' => $line_items,
+            'mode' => 'payment',
+            'success_url' => route('checkout.success'),
+            'cancel_url' => route('checkout.index'),
+        ]);
+
+        return redirect($checkout_session->url);
+    }
+
+    public function success()
+    {
+        return view('checkout.success');
+    }
 }
